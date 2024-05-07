c6515e4726738f916da3b6f2638aae3ac7b28ac1
Hichanmin <pcm4106@naver.com>
2024-05-07 16:34:39 +0900

    likeChek 임시 구현 1차

---
+++ src/main/java/com/example/member/mapper/LikeMapper.java
@@ -0,0 +1,12 @@
+package com.example.member.mapper;
+
+import com.example.member.dto.LikeDTO;
+import com.example.member.entity.LikeEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+public interface LikeMapper {
+    LikeMapper INSTANCE = Mappers.getMapper(LikeMapper.class);
+
+    LikeEntity toEntity(LikeDTO likeDTO);
+    LikeDTO toDTO(LikeEntity likeEntity);
+}
--- src/main/java/com/example/member/mapper/TodoMapper.java
+++ src/main/java/com/example/member/mapper/TodoMapper.java
@@ -1,8 +1,6 @@
 package com.example.member.mapper;
 
-import com.example.member.dto.MemberDTO;
 import com.example.member.dto.TodoDTO;
-import com.example.member.entity.MemberEntity;
 import com.example.member.entity.TodoEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
